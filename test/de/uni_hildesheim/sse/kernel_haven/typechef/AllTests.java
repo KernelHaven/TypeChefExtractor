55bedd9b1f04352dda1ed33d243d1e036e7fb495
Adam <adam@sse.uni-hildesheim.de>
2017-05-09 12:50:38 +0200

    Add dummy test case

---
+++ test/de/uni_hildesheim/sse/kernel_haven/typechef/AllTests.java
@@ -0,0 +1,13 @@
+package de.uni_hildesheim.sse.kernel_haven.typechef;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    TypeChefExtractorTest.class
+    })
+public class AllTests {
+
+}
---
+++ test/de/uni_hildesheim/sse/kernel_haven/typechef/TypeChefExtractorTest.java
@@ -0,0 +1,11 @@
+package de.uni_hildesheim.sse.kernel_haven.typechef;
+
+import org.junit.Test;
+
+public class TypeChefExtractorTest {
+
+    @Test
+    public void dummy() {
+    }
+    
+}
